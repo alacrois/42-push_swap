589178f569dbff022a158a017b9dceffe6420cbb
alacrois <alacrois@student.42.fr>
2020-07-28 18:56:20 +0200

    ..

--- test_shuffle_timed.sh
+++ test_shuffle_timed.sh
@@ -3,6 +3,5 @@ if [ $# -eq 1 ]
 then
 	arg=`ruby -e "puts (-$1...$1).to_a.shuffle.join(' ')"`
 	echo "arg = $arg"
-	time ./push_swapV5 $arg | ./checker -d -c $arg
 	time ./push_swap $arg | ./checker -d -c $arg
 fi
